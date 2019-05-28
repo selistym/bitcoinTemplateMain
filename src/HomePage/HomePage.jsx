17412e0cb1728278fba5385a9e5f77082a5ea075
selistym <selistym@protonmail.com>
2019-05-28 10:10:24 +0300

    add fixed table header

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -16,14 +16,14 @@ const { Content } = Layout;
 import './HomePage.css';
 
 function numberWithCommas(x) {
-    // return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");    
-    var parts = x.toString().split(".");
-    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    return parts.join(".");
+  // return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");    
+  var parts = x.toString().split(".");
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return parts.join(".");
 }
 
-function numberwith6decimals(x){
-  if(parseFloat(x).toFixed(6)!=parseFloat(x)){
+function numberwith6decimals(x) {
+  if (parseFloat(x).toFixed(6) != parseFloat(x)) {
     return parseFloat(x).toFixed(6);
   }
   return x;
@@ -46,127 +46,140 @@ export const HomePage = () => {
   }, [fetched]);
 
   const coinColumnsShort = [
-  {
-    title:'#',
-    dataIndex:'mc_rank',
-    key:'1',
-    sorter: (a, b) => a.mc_rank - b.mc_rank,
-  },
+    {
+      title: '#',
+      dataIndex: 'mc_rank',
+      key: '1',
+      sorter: (a, b) => a.mc_rank - b.mc_rank,
+      width: 100,
+    },
     {
       title: 'Asset Name',
       dataIndex: 'coin_title',
-      key: '2',             
+      key: '2',
       sorter: (a, b) => a.coin_title.localeCompare(b.coin_title),
       render: (volume, row) => (
         <div><img src={loading} data-src={row.img_url} width="20" height="20" /> {row.coin_title}</div>
-      )
+      ),
+      width: 180,
     },
-    ,{
+    , {
       title: 'Market Cap',
       dataIndex: 'market_cap',
       key: '3',
       sorter: (a, b) => a.market_cap - b.market_cap,
       render: (volume, row) => {
-        if(row.market_cap)
-        return '$' + numberWithCommas(parseInt(row.market_cap).toFixed(0) || 0);
-      }
-    },{
+        if (row.market_cap)
+          return '$' + numberWithCommas(parseInt(row.market_cap).toFixed(0) || 0);
+      },
+      width: 180,
+    }, {
       title: 'Price',
       dataIndex: 'asset_price',
       key: '4',
-      render: (volume, row) =>{
-        if(row.asset_price)
-          return '$' + numberwith6decimals(row.asset_price || 0);        
+      render: (volume, row) => {
+        if (row.asset_price)
+          return '$' + numberWithCommas(numberwith6decimals(row.asset_price || 0));
       },
       sorter: (a, b) => a.asset_price - b.asset_price,
-    },{
+      width: 180,
+    }, {
       title: 'Price Change (24H)',
       dataIndex: 'asset_price_old',
       key: '5',
       render: (price, row) => {
-        if(row.asset_price)
+        if (row.asset_price)
           return getPriceChange(row.asset_price, price) + '%'
       },
-      sorter: (a, b) => getPriceChange(a.asset_price, a.asset_price_old) - getPriceChange(b.asset_price, b.asset_price_old)
-    },{
+      sorter: (a, b) => getPriceChange(a.asset_price, a.asset_price_old) - getPriceChange(b.asset_price, b.asset_price_old),
+      width: 180,
+    }, {
       title: 'Volume (24H)',
       dataIndex: 'volume_24_old',
-      key: '6',      
-      render: (volume, row) =>{
-        if(row.volume_24_old)
+      key: '6',
+      render: (volume, row) => {
+        if (row.volume_24_old)
           return "$" + numberWithCommas(parseInt(row.volume_24_old) || 0);
       },
       sorter: (a, b) => a.volume_24_old - b.volume_24_old,
+      width: 180,
     },
     {
       title: 'Volume Change',
       dataIndex: 'volume_24',
       key: '7',
       render: (volume, row) => {
-        if(row.volume_24)
+        if (row.volume_24)
           return getVolumnChange(volume, row.volume_24_old) + '%';
       },
-      sorter: (a, b) => getVolumnChange(a.volume_24, a.volume_24_old) - getVolumnChange(b.volume_24, b.volume_24_old)
+      sorter: (a, b) => getVolumnChange(a.volume_24, a.volume_24_old) - getVolumnChange(b.volume_24, b.volume_24_old),
+      width: 180,
     },
     {
       title: 'All Time High',
       dataIndex: 'ath',
       key: '8',
-      render: (volume, row) =>{
-        if(row.ath)
+      render: (volume, row) => {
+        if (row.ath)
           return '$' + numberWithCommas(numberwith6decimals(row.ath));
       },
       sorter: (a, b) => a.ath - b.ath,
+      width: 180,
     },
     {
       title: 'All Time Low',
       dataIndex: 'atl',
       key: '9',
-      render: (volume, row) =>{
-        if(row.atl)
-        return '$' + numberwith6decimals(row.atl);
+      render: (volume, row) => {
+        if (row.atl)
+          return '$' + numberwith6decimals(row.atl);
       },
       sorter: (a, b) => a.atl - b.atl,
-    },    {
+      width: 180,
+    },
+    {
       title: 'Buy Support 5%',
       dataIndex: 'buy_support_5',
       key: '10',
       render: (volume, row) => {
-        if(row.buy_support_5)
-        return '$' + numberWithCommas(Number.parseInt(row.buy_support_5) || 0);
+        if (row.buy_support_5)
+          return '$' + numberWithCommas(Number.parseInt(row.buy_support_5) || 0);
       },
       sorter: (a, b) => a.buy_support_5 - b.buy_support_5,
+      width: 180,
     },
     {
       title: 'Sell Support 5%',
       dataIndex: 'sell_support_5',
       key: '11',
       render: (volume, row) => {
-        if(row.sell_support_5)
-        return '$' + numberWithCommas(Number.parseInt(row.sell_support_5) || 0 );
+        if (row.sell_support_5)
+          return '$' + numberWithCommas(Number.parseInt(row.sell_support_5) || 0);
       },
       sorter: (a, b) => a.sell_support_5 - b.sell_support_5,
+      width: 180,
     },
     {
       title: 'TA Rating',
       dataIndex: 'ta_rating',
       key: '12',
       sorter: (a, b) => a.ta_rating - b.ta_rating,
+      width: 180,
     },
     {
       title: 'Volatility 30 days',
       dataIndex: 'volatility_30_usd',
       key: '13',
       render: (volume, row) => {
-        if(row.volatility_30_usd)
-        return Number.parseFloat(row.volatility_30_usd).toFixed(2);
+        if (row.volatility_30_usd)
+          return Number.parseFloat(row.volatility_30_usd).toFixed(2);
       },
-      sorter: (a, b) => a.volatility_30_usd - b.volatility_30_usd,
+      sorter: (a, b) => a.volatility_30_usd - b.volatility_30_usd,      
     },
   ];
 
   // sort coins by mc_rank
-  coins.sort(function(a,b){return a.mc_rank - b.mc_rank});
+  coins.sort(function (a, b) { return a.mc_rank - b.mc_rank });
 
   return (
     <div>
@@ -174,8 +187,14 @@ export const HomePage = () => {
         <Navigation activeNav="1" />
         <Layout>
           <CustomHeader />
-          <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280}}>
-            <Table className="homeTable" loading={coins.length > 0 ? false : true} rowKey={coin => coin.coin_id} pagination={false} dataSource={coins} columns={coinColumnsShort} size="small" />
+          <Content style={{margin: '24px', padding:'30px', background: '#fff'}}>
+            <Table className="homeTable" 
+              loading={coins.length > 0 ? false : true} 
+              rowKey={coin => coin.coin_id}
+              scroll={{y: 1050}}
+              pagination={false} 
+              dataSource={coins} 
+              columns={coinColumnsShort} />
           </Content>
         </Layout>
       </Layout>
